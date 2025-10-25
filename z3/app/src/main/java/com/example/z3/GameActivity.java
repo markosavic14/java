b9333923e345bf43da6776b2d8928cb832ebbf61
Marko Savic <markosavic14@gmail.com>
2025-10-25 16:26:44 +0200

    renamed mainactivity to gameactivity to avoid confusion

--- z3/app/src/main/java/com/example/z3/GameActivity.java
+++ z3/app/src/main/java/com/example/z3/GameActivity.java
@@ -11,7 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     final int NUMBER_OF_ROWS = 7;
 
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
